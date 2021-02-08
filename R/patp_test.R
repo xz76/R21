6ee49200a5e29c9c35cbba277f809e75a0a17dcf
Xuan Zhang <xuan.zhang@uconn.edu>
2021-02-07 21:33:41 -0500

    Add cid and group to data simulation

--- R/patp_test.R
+++ R/patp_test.R
@@ -3,10 +3,138 @@
 ## of transition probabilities based on a Kolmogorov-Smirnov-type
 ## test
 
-patp_test <- function(data, tmat, cid, id, group, h, j, s=0,
+simulate <- function(n=1000, a12=0.5, a13=1, a21=0.75 ,a23=0.5,
+                     p12=1, p13=1, p21=1, p23=1, c.rate=1, b.Z.cens=0.5,
+                     p.Z=0.4, b.Z=0.5, gamma_0=-1, gamma_1=1) {
+  # In this function when p12=p13=p21=p23=1 we have a homogeneous process
+  # (i.e. time-constant intensities)
+  
+  outdata <- NULL
+  for(i in 1:n){
+    #Simulate covariate that is associated with X(t), right censoring, and missingness
+    Z <- rbinom(n=1, size=1, prob=p.Z)
+    
+    #Probability of missingness
+    p.miss <- exp(gamma_0 + gamma_1*Z)/(1+exp(gamma_0 + gamma_1*Z))
+    #R <- rbinom(n=1, size=1, prob=p.miss)
+    R=1
+    if(R==1){
+      
+      CC <- rexp(1, rate=c.rate*exp(b.Z.cens*Z))
+      
+      t12 <- rweibull(1, shape=p12, scale=(1/(a12*exp(b.Z*Z))))
+      t13 <- rweibull(1, shape=p13, scale=(1/(a13*exp(b.Z*Z))))
+      stop <- 1*((t13 < t12) | (CC < t12)) #Stop if death or censoring first
+      x1 <- min(c(t12, t13)) # Exit time from state 1 (ignoring censoring)
+      if (stop==0){
+        t1 <- 0
+        t2 <- x1
+        s1 <- 1
+        s2 <- 2
+        while (stop==0){
+          if(s2[length(s2)]==2){
+            t23 <- rtrunc(1, spec="weibull", a=t2[length(t2)], b=Inf,
+                          shape=p23, scale=(1/(a23*exp(b.Z*Z))))
+            t21 <- rtrunc(1, spec="weibull", a=t2[length(t2)], b=Inf,
+                          shape=p21, scale=(1/(a21*exp(b.Z*Z))))
+            
+            stop <- 1*((t23 < t21) | (CC < t21)) #Stop if death or censoring first
+            x2 <- min(c(t21, t23)) # Exit time from state 2 (ignoring censoring)
+            
+            t1 <- c(t1, x1)
+            t2 <- c(t2, min(x2, CC))
+            s1 <- c(s1, 2)
+            s2 <- c(s2, ifelse(x2 <= CC, ifelse(t21 <= t23, 1, 3), 2))
+          } else {
+            t12 <- rtrunc(1, spec="weibull", a=t2[length(t2)], b=Inf,
+                          shape=p12, scale=(1/(a12*exp(b.Z*Z))))
+            t13 <- rtrunc(1, spec="weibull", a=t2[length(t2)], b=Inf,
+                          shape=p13, scale=(1/(a13*exp(b.Z*Z))))
+            
+            stop <- 1*((t13 < t12) | (CC < t12)) #Stop if death or censoring first
+            x1 <- min(c(t12, t13)) # Exit time from state 1 (ignoring censoring)
+            
+            t1 <- c(t1, x2)
+            t2 <- c(t2, min(x1, CC))
+            s1 <- c(s1, 1)
+            s2 <- c(s2, ifelse(x1 <= CC, ifelse(t12 <= t13, 2, 3), 1))
+          }
+        }
+      } else {
+        t1 <- 0
+        t2 <- min(x1, CC)
+        s1 <- 1
+        s2 <- ifelse(x1 < CC, 3, 1)
+      }
+    } else {
+      t1 <- 0
+      t2 <- NA
+      s1 <- 0
+      s2 <- NA
+    }
+    
+    id <- rep(i, length(t1))
+    Z <- rep(Z, length(t1))
+    R <- rep(R, length(t1))
+    outdata <- rbind(outdata, data.frame(id, t1, t2, s1, s2, Z, R))
+  }
+  # t1: start time of the interval
+  # t2: end time of the interval
+  # s1: state at the start time of the interval
+  # s2: state at the end time of the interval
+  # if s1==s2 then right censoring occured at t2 at this interval
+  outdata <- outdata[order(outdata$id, outdata$t1, outdata$t2),]
+  return(outdata)
+}
+
+#######################################
+#### Add group and cid information#####
+#######################################
+sim_group <- function(data, cid){
+  l <- length(unique(data$id))
+  g <- rbinom(l, 1, 0.5)
+  q <- quantile(l)
+  rand_cid <- function(cid, l, sd = 1) {
+    vec <- rnorm(cid - 1, l/(cid), sd)
+    vec <-  floor(vec)
+    vec[cid] <- l - sum(vec)
+    vec
+  }  
+  cid_seq <- rand_cid(cid = cid, l = l ,sd = 1)
+  ## Add group information
+  res <- do.call(rbind, by(data, data$id, function(sub_dat){
+    sub_dat$group <- rep(g[sub_dat$id[1]], nrow(sub_dat))
+    return(sub_dat)
+  }))
+  cid_num <- NULL
+  ## Add cid information
+  for(i in 1 : length(cid_seq)){
+    tmp <- replicate(cid_seq[i], i)
+    cid_num <- c(cid_num,tmp)
+  }
+  out <- do.call(rbind, by(res, res$id, function(sub_dat){
+    sub_dat$cid <- rep(cid_num[sub_dat$id[1]], nrow(sub_dat))
+    return(sub_dat)
+  }))
+  rownames(out) <- NULL
+  return(out)
+}
+
+tmat1 <- transMat(x = list(c(2, 3), c(3), c()), names = c("Health", "Illness", "Death"))
+tmp <- simulate(100)
+tmp <- sim_group(tmp, cid = 3)
+data <- tmp
+
+patp_test <- function(data, tmat = tmat1, cid, id, group, h, j, s=0,
                       weighted=FALSE, LMAJ=FALSE, B=1000){
-  check.ic <- aggregate(data[,cid], by=list(data[,id]), 
-                        FUN=sd, na.rm=TRUE)$x
+  # microbenchmark::microbenchmark(aggregate(data[,cid], by=list(data[,id]), 
+  #                                          FUN=sd, na.rm=TRUE)$x,
+  #                                tapply(data[, cid], data[, id], sd, na.rm = TRUE))
+  # check.ic <- aggregate(data[,cid], by=list(data[,id]), 
+  #                       FUN=sd, na.rm=TRUE)$x
+  ## tapply faster than aggregate function.
+  
+  check.ic <- tapply(data[, cid], data[, id], sd, na.rm = TRUE)
   check.ic <- check.ic[!is.na(check.ic)]
   if(length(check.ic)>0){
     if(max(check.ic)>0){
@@ -32,7 +160,7 @@ patp_test <- function(data, tmat, cid, id, group, h, j, s=0,
                               data[,group],
                               data[,id]),
                     FUN = mean)
-    
+    M0 <- tapply(rep(1,times=nrow(data)), data[])
     M <- aggregate(M0$x,
                    by = list(M0$Group.1, M0$Group.2),
                    FUN = sum)
@@ -62,9 +190,12 @@ patp_test <- function(data, tmat, cid, id, group, h, j, s=0,
           A0$Haz[A0$Haz$trans==trn,"Haz"] <- fun(A0$Haz[A0$Haz$trans==trn,"time"])
         }
       }
+      #######
+      P0 <- sop_tnew()
+      ######
       
-      P0 <- probtrans(A0, predt = s, variance=FALSE)[[h]][,c("time",
-                                                             paste("pstate", j, sep=""))]
+      P0 <- probtrans(A0, predt = s,
+                      variance=FALSE)[[h]][,c("time", paste("pstate", j, sep=""))]
     } else {
       P0 <- LMAJ2(msdata=data[data[,group]==g,], tmat=tmat, 
                   id=id, s=s, h=h, j=j, weighted=weighted)
@@ -125,5 +256,3 @@ patp_test <- function(data, tmat, cid, id, group, h, j, s=0,
   names(pval) <- "p-value"
   return(pval)
 }
-
-
